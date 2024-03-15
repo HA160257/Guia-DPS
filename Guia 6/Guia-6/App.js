fd314f9cc64031b59b7dacf9ff81e9098d0a009b
Carlos Vásquez <mikemaster2023@gmail.com>
2024-03-15 15:06:19 -0600

    La Guia 6

---
+++ Guia 6/Guia-6/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, StyleSheet,Image} from
+'react-native';
+const App = () =>{
+return(
+<>
+<View >
+<View style={{flexDirection:'row'}} >
+<Image
+style={styles.banner}
+source={require('./src/img/bg.jpg')} />
+</View>
+</View>
+</>
+);
+};
+const styles = StyleSheet.create({
+banner:{
+height:250,
+flex:1
+}
+});
+export default App;
